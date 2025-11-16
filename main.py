9a305e92ac769fecff4cf367bb3220e89f6ff2ab
mekiisupertramp <mehmed.blazevic@gmail.com>
2025-11-16 22:31:48 +0100

    Updated the RGB driver and added modes

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-from machine import Pin
+from machine import Pin, PWM
 import network    
 import time
 import socket
-import neopixel
+import rp2
+import array
 
-pin = Pin(0)
-ledQty = 2
-strip = neopixel.NeoPixel(pin,ledQty)
+
+pin = Pin(22, Pin.OUT)
+ledQty = 140
 
 led = Pin("LED", Pin.OUT)
-red = 180
+red = 0
 green = 0
-blue = 150
+blue = 20
+mode = 1
+
+
+@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
+def ws2812():
+    T1 = 2 #2
+    T2 = 5 #5
+    T3 = 3 #3
+    wrap_target()
+    label("bitloop")
+    out(x, 1) .side(0) [T3 - 1]
+    jmp(not_x, "do_zero") .side(1) [T1 - 1]
+    jmp("bitloop") .side(1) [T2 - 1]
+    label("do_zero")
+    nop() .side(0) [T2 - 1]
+    wrap()
+
+# Create the StateMachine with the ws2812 program
+# GREEN RED BLUE
+sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=pin)
+# Start the StateMachine, it will wait for data on its FIFO.
+sm.active(1)
+
+def putRGB(num,color):
+    ar = array.array("I", [0 for _ in range(ledQty)])
+    ar[num] = color
+    sm.put(ar,8)
+
+def putRGBs(color):
+    ar = array.array("I", [0 for _ in range(ledQty)])
+    for i in range(ledQty):
+        ar[i] = color
+    sm.put(ar,8)
+        
+
 
 # Wi-Fi credentials
 ssid = 'Sunrise_4513373'
@@ -70,6 +106,14 @@ html = """<!DOCTYPE html>
         Blue:
         <input type="range" min="0" max="255" value={} class="slider" id="slideB" name="BLUE">        
     </div>
+    <div>
+        Mode 1:
+        <input type="radio" value=1 name="mode" {}>
+        Mode 2:
+        <input type="radio" value=2 name="mode" {}>
+        Mode 3:
+        <input type="radio" value=3 name="mode" {}>        
+    </div>
     </form>
 </body>
 </html>
@@ -85,6 +129,13 @@ s.listen(1)
 
 print('Listening on', addr)
 
+
+i = 0
+for i in range(30):
+    time.sleep_ms(30)
+    led.value(i%2)
+    i=i+1
+
 while True:        
     # Accept a connection
     cl, addr = s.accept()
@@ -93,20 +144,19 @@ while True:
     # Receive the request
     request = cl.recv(2048)
     request_str = str(request)
-    print(request_str)
+    print(request_str)    
     
     # Check if it's a GET request to toggle the LED
     if '/setrgb' in request_str:
         if request_str.find('RED=') != -1:
             red = int(request_str.split('RED=')[1].split('&')[0])
             print('value of red is: ', red)
-            blue = int(request_str.split('BLUE=')[1].split("'")[0])
+            blue = int(request_str.split('BLUE=')[1].split("&")[0])
             print('value of blue is: ', blue)
             green = int(request_str.split('GREEN=')[1].split('&')[0])
-            print('value of green is: ', green)
-            strip[0] = (blue, red, green)
-            strip[1] = (blue, red, green)
-            strip.write()
+            print('value of green is: ', green)                         
+            mode = int(request_str.split('mode=')[1].split("'")[0])
+            print('value of mode is: ', mode)
             led.value(1)
             time.sleep_ms(200)
             led.value(0)
@@ -114,12 +164,33 @@ while True:
         led.value(1)
     if '/ledoff' in request_str:
         led.value(0)
+
+    if mode == 1:
+        putRGBs(green << 16 | red << 8 | blue)
+    elif mode == 2:
+        for i in range(10):
+            time.sleep_ms(200)
+            putRGBs(green << 16 | red << 8 | blue)
+            time.sleep_ms(200)
+            putRGBs(0)
+    elif mode == 3:
+        for i in range(30):
+            time.sleep_ms(30)
+            putRGBs(green << 16 | red << 8 | blue)
+            time.sleep_ms(30)
+            putRGBs(0)
     
     # Send back the HTML response with the current state of the LED
-    response_html = html.format("ON" if led.value() else "OFF", red, green, blue)
+    if mode == 1:        
+        response_html = html.format("ON" if led.value() else "OFF", red, green, blue, "checked", "unchecked", "unchecked")
+    elif mode == 2:
+        response_html = html.format("ON" if led.value() else "OFF", red, green, blue, "unchecked", "checked", "unchecked")
+    elif mode == 3:
+        response_html = html.format("ON" if led.value() else "OFF", red, green, blue, "unchecked", "unchecked", "checked")
+    #print(response_html)
     
     cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
     cl.send(response_html)
     
     cl.close()
-
+    
